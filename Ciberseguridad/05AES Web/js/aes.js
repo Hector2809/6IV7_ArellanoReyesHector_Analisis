0297db2c40f07af9c32debcf14c4c68f8de1b20e
Hector2809 <arellano.reyes.hector@gmail.com>
2025-04-13 23:55:51 -0600

    Metodo AES

--- Ciberseguridad/05AES Web/js/aes.js
+++ Ciberseguridad/05AES Web/js/aes.js
@@ -7,15 +7,17 @@ function cifrar(){
     if (mensaje === "" || password === "") {
         alert("Por favor, llena todos los campos.");
         return;
-    }else if (password.length() != 16 || password.length() != 24 || password.length() != 32) {
+    }
+    if (password.length() != 16 || password.length() != 24 || password.length() != 32) {
         alert("La clave debe tener al menos 16, 24 o 32 caracteres.");
         return;
-    }else if (mensaje.length() != 16 || mensaje.length() != 24 || mensaje.length() != 32){
+    }
+    if (mensaje.length() != 16 || mensaje.length() != 24 || mensaje.length() != 32){
         alert("El texto debe tener al menos 16, 24 o 32 caracteres.")
         return;
-    }else{
-    document.getElementById("texto_cifrado").textContent = "El texto cifrado es: " + cifrado;
     }
+
+    document.getElementById("texto_cifrado").textContent = "El texto cifrado es: " + cifrado;
 }
 
 function descifrar(){
@@ -27,21 +29,20 @@ function descifrar(){
     if (mensaje === "" || password === "") {
         alert("Por favor, llena todos los campos.");
         return;
-    }else if (password.length() != 16 || password.length() != 24 || password.length() != 32) {
+    }
+    if (password.length() != 16 || password.length() != 24 || password.length() != 32) {
         alert("La clave debe tener al menos 16, 24 o 32 caracteres.");
         return;
-    }else{
-        try{
-            var descifrado = CryptoJS.AES.decrypt(mensaje, password);
-            var textoFormateado = descifrado.toString(CryptoJS.enc.Utf8);
-            if (!textoFormateado) {
-                throw new Error("La clave es incorrecta o el texto no es válido.");
-            }else{
-                document.getElementById("texto_descifrado").textContent = "El texto descifrado sin formato es: " + descifrado;
-                document.getElementById("texto_descifrado_formato").textContent = "El texto descifrado con formato es: " + descifrado.toString(CryptoJS.enc.Utf8);
-            }    
+    }
+    try{
+        var descifrado = CryptoJS.AES.decrypt(mensaje, password);
+        var textoFormateado = descifrado.toString(CryptoJS.enc.Utf8);
+        if (!textoFormateado) {
+            throw new Error("La clave es incorrecta o el texto no es válido.");
+        }
+        document.getElementById("texto_descifrado").textContent = "El texto descifrado sin formato es: " + descifrado;
+        document.getElementById("texto_descifrado_formato").textContent = "El texto descifrado con formato es: " + descifrado.toString(CryptoJS.enc.Utf8);   
         }catch(e){
             alert("Error al descifrar. Asegúrate de que la clave y el mensaje sean correctos.");
         }
-    }
 }
